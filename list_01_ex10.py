d64c5b3a82310122254219e61071bf8e840bafda
FelipeSantos-ADS <felipesilveira302007@gmail.com>
2026-09-23 22:16:42 -0300

    Delete list_01_ex10.py

--- list_01_ex10.py
+++
@@ -1,5 +0,0 @@
-# Receba 2 números reais. Calcule e mostre a diferença desses valores
-x = float(input("Digite o 1° número: "))
-y = float(input("Digite o 2° número: "))
-diferenca = x - y
-print("Diferença:", diferenca)
